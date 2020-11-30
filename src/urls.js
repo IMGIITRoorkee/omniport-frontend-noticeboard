8d6e4ec9a930266a201b495271e23468244b8462
Prakhar <gprakhar11@gmail.com>
2020-11-30 15:32:13 +0530

    Add baseNavUrl function

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,5 @@
+import config from '../config.json'
+
 //Backend
 export const baseApiUrl = () => {
     return `/api/noticeboard/`
@@ -6,4 +8,9 @@ export const baseApiUrl = () => {
 export const noticesUrl = (page) => {
     let url = `${baseApiUrl()}new/?page=${page}`
     return url
+}
+
+//Frontend
+export const baseNavUrl = forwardLink => {
+    return `${config.baseUrl}${forwardLink}`
 }
