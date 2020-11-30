0fcca0328298a89caa03d5e419782081736fbc90
Prakhar <gprakhar11@gmail.com>
2020-11-30 15:23:52 +0530

    Change hardcode no of notices to variable

--- src/actions/getNotices.js
+++ src/actions/getNotices.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import { toast } from 'react-semantic-toasts'
 import { noticesUrl } from '../urls'
+import { no_of_notices } from '../const'
 
 export const getNotices = (page) =>  (dispatch) => {
     axios
         .get(noticesUrl(page))
         .then(response => {
-            // dispatch(console.log(response.data.results))
-            let totalPages = Math.ceil(respone.data.results.count / 10)
+            // dispatch(console.log(no_of_notices))
+            let totalPages = Math.ceil(response.data.count / no_of_notices)
             if (totalPages === 0) {
                 // The total pages can't be 0 in Pagination component
                 totalPages = 1
