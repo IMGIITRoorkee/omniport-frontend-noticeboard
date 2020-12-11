a65f777d33913e9f08fe31f5deee9fa84a890421
Prakhar <gprakhar11@gmail.com>
2020-12-11 15:54:45 +0530

    Capture variables for fetching notices using getState()

--- src/actions/getNotices.js
+++ src/actions/getNotices.js
@@ -7,6 +7,7 @@ import {
     dateFilterUrl
 } from '../urls'
 import { toast } from 'react-semantic-toasts'
+import { no_of_notices } from '../const'
 
 function requestNotices(page, searchKeyword) {
     return {
@@ -28,7 +29,7 @@ function receiveNotices(
     mainCategorySlug,
     showImp
 ) {
-    let totalPages = Math.ceil(noticeDataList.count / 10)
+    let totalPages = Math.ceil(noticeDataList.count / no_of_notices)
     if (totalPages === 0) {
         // The total pages can't be 0 in Pagination component
         totalPages = 1
@@ -51,78 +52,76 @@ function receiveNotices(
     }
 }
 
-export const getNotices = (
-    page,
-    searchKeyword,
-    narrowBookmark,
-    expired,
-    bannerId,
-    mainCategorySlug,
-    dateRange,
-    showImp
-) => {
-    return dispatch => {
-        dispatch(requestNotices(page, searchKeyword))
-        let url
-        if (expired) {
-            url = expiredNoticesUrl(page, searchKeyword)
-        } else if (dateRange) {
-            url = dateFilterUrl(
-                page,
-                dateRange.start,
-                dateRange.end,
-                bannerId,
-                mainCategorySlug,
-                searchKeyword
-            )
-        } else if (bannerId) {
-            url = filterUrl(page, bannerId, searchKeyword, mainCategorySlug)
-        } else if (narrowBookmark) {
-            url = bookmarkedNoticesUrl(page)
-        } else if (showImp) {
-            url = importantNoticesUrl(page)
-        } else {
-            url = noticesUrl(page, searchKeyword)
-        }
+export const getNotices = () => (dispatch, getState) => {
+    const page = getState().notices.page
+    const searchKeyword = getState().notices.searchKeyword
+    const narrowBookmark = getState().notices.narrowBookmark
+    const expired = getState().notices.expired
+    const bannerId = getState().notices.bannerId
+    const mainCategorySlug = getState().notices.mainCategorySlug
+    const dateRange = getState().notices.dateRange
+    const showImp = getState().notices.showImp
 
-        return fetch(url)
-            .then(response => response.json())
-            .then(json =>
-                dispatch(
-                    receiveNotices(
-                        json,
-                        searchKeyword,
-                        narrowBookmark,
-                        expired,
-                        bannerId,
-                        dateRange,
-                        mainCategorySlug,
-                        showImp
-                    )
+    dispatch(requestNotices(page, searchKeyword))
+    let url
+    if (expired) {
+        url = expiredNoticesUrl(page, searchKeyword)
+    } else if (dateRange) {
+        url = dateFilterUrl(
+            page,
+            dateRange.start,
+            dateRange.end,
+            bannerId,
+            mainCategorySlug,
+            searchKeyword
+        )
+    } else if (bannerId) {
+        url = filterUrl(page, bannerId, searchKeyword, mainCategorySlug)
+    } else if (narrowBookmark) {
+        url = bookmarkedNoticesUrl(page)
+    } else if (showImp) {
+        url = importantNoticesUrl(page)
+    } else {
+        url = noticesUrl(page, searchKeyword)
+    }
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(json =>
+            dispatch(
+                receiveNotices(
+                    json,
+                    searchKeyword,
+                    narrowBookmark,
+                    expired,
+                    bannerId,
+                    dateRange,
+                    mainCategorySlug,
+                    showImp
                 )
             )
-            .catch(err => {
-                dispatch({
-                    type: 'FETCH_NOTICES_FAILURE'
-                })
-                if (err.response) {
-                    err.response.data
-                        ? toast({
-                            type: 'error',
-                            title: 'Failed to fetch notices!',
-                            description: err.response.data.msg
-                        })
-                        : toast({
-                            type: 'error',
-                            title: 'Failed to fetch notices!',
-                            description: err.response.statusText
-                        })
-                } else {
-                    toast({
-                        type: 'error',
-                        title: 'Failed to fetch notices!'
-                    })
-                }
+        )
+        .catch(err => {
+            dispatch({
+                type: 'FETCH_NOTICES_FAILURE'
             })
-    }
+            if (err.response) {
+                err.response.data
+                    ? toast({
+                        type: 'error',
+                        title: 'Failed to fetch notices!',
+                        description: err.response.data.msg
+                    })
+                    : toast({
+                        type: 'error',
+                        title: 'Failed to fetch notices!',
+                        description: err.response.statusText
+                    })
+            } else {
+                toast({
+                    type: 'error',
+                    title: 'Failed to fetch notices!'
+                })
+            }
+        })
 }
